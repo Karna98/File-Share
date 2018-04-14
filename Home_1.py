f25f6a0521c6d28cd5389d38784683a6f60a4e20
Karna98 <vedantwakalkar@gmail.com>
2018-04-15 00:41:03 +0530

    Updated Senders part with remote server sharing using local server

--- Home_1.py
+++ Home_1.py
@@ -1,12 +1,18 @@
-from tkinter import *
 import tkinter
 import subprocess
+from tkinter import *
 from PIL import ImageTk,Image
 
 root = tkinter.Tk()
 root.title("Share File!")
 root.geometry("500x500")
 root.configure(background='#CD8C95')
+
+canvas = Canvas(root, width = 500, height = 250)
+line = canvas.create_line(0,251,500,251)
+canvas.pack()
+img = ImageTk.PhotoImage(Image.open("icon.jpg"))
+canvas.create_image(0, 0, anchor=NW, image=img)
 
 def call_send():
     subprocess.call(" python part_1.py 1", shell=True)
@@ -15,17 +21,11 @@ def call_send():
 def call_receive():
     subprocess.call(" python part_2.py 1", shell=True)
     quit()
-canvas = Canvas(root, width = 500, height = 250)
-line = canvas.create_line(0,251,500,251)
-canvas.pack()
-img = ImageTk.PhotoImage(Image.open("icon.jpg"))
-canvas.create_image(0, 0, anchor=NW, image=img)
 
 B = tkinter.Button(root,bg= '#FFB6C1',bd = 5, text ="Send a file",command = call_send,width = 20)
 B.pack(side=LEFT, padx=50, pady=5)
-#B.place(x=.250, y=.250)
+
 B = tkinter.Button(root,bg= '#FFB6C1',bd = 5, text ="Receive a file",command = call_receive,width=20)
 B.pack(side=RIGHT, padx=50, pady=5)
-#B.place(x=.550, y=.550)
 
 root.mainloop()
--- part_2.py
+++ part_2.py
@@ -1,14 +1,10 @@
-from tkinter import *
 import tkinter
 import subprocess
 import urllib.request
-from PIL import ImageTk,Image
 import webbrowser
+from tkinter import *
+from PIL import ImageTk , Image
 
-def go():
-    inputValue = textBox.get("1.0", "end-1c")
-    print(inputValue)
-    webbrowser.open(inputValue)
 
 root = tkinter.Tk()
 root.title("Share File!")
@@ -21,12 +17,21 @@ canvas.pack()
 img = ImageTk.PhotoImage(Image.open("icon.jpg"))
 canvas.create_image(0, 0, anchor=NW, image=img)
 
+def go():
+    inputValue = textBox.get("1.0", "end-1c")
+    print(inputValue)
+    webbrowser.open(inputValue)
+
 label = Label(root, text= 'Enter URL shared by Sender(ex. 127.0.0.101:8000)')
 label.pack(side=TOP,padx=30,pady=20)
+
 textBox=Text(root, height=1, width=20)
 textBox.pack()
+
 button = Button(root,bd = 5,bg= '#FFB6C1', text='Go', command =  lambda: go())
 button.pack(side=TOP, padx=45, pady=10)
 
+button = Button(root,bd = 5,bg= '#FFB6C1', text='Quit Application :-(', command =  quit)
+button.pack(side=TOP, padx=45, pady=10)
 
 root.mainloop()
